58384aeeb44e45ac141bd6ff058c206197ec0b39
Nir Mathur <nirmathur@Nirs-Mac-mini.local>
2025-07-31 18:59:50 +0400

    chore(ci): add black/isort pre-commit hooks

--- agent/core/governer.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-
-MAX_SPECTRAL_RADIUS = 0.9  # charter clause x^x-17
-
-def run_one_cycle():
-    """Minimal no-op cycle that logs completion."""
-    logger.info("cycle-complete")
-    return True
